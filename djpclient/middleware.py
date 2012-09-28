64bdfe7d559ea54846f9bc0adcc1b5951adaf9bd
Adam Hammouda <adamhammouda3@gmail.com>
2012-09-27 19:39:30 -0500

    Some logging for heroku sessions

--- djpclient/middleware.py
+++ djpclient/middleware.py
@@ -81,6 +81,10 @@ class DJPClientMiddleware(object):
                 s.save()
             else:
                 print 'already set .. .. ..'
+                print s.session_data
+                print type(s.session_data)
+                print ast.literal_eval(s.session_data)
+                print type(ast.literal_eval(s.session_data))
                 cookie_val = ast.literal_eval(s.session_data)['ga-report-id']
                 cookie_expire = s.expire_date
         
